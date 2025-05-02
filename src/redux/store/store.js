a1c08c8266d2ab30adf32ee6fa53d91819b73b4b
Jose Manuel Bono Campos <jose542.ceus@gmail.com>
2025-05-02 19:15:14 +0200

    fix: error

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import guestsReducer from "./../features/guests/guestsSlice";
+import roomsReducer from "./../features/rooms/roomsSlice";
 
 const store = configureStore({
   reducer: {
     guest: guestsReducer, // Aquí pasamos el reducer correctamente
+    room: roomsReducer, // Aquí pasamos el reducer correctamente
   },
 });
 

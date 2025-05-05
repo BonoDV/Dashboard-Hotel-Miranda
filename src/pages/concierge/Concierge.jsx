6e34040698950adec8bdbb667436d95b378581b3
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-05-05 13:40:47 +0200

    feature: implemented concierge slice

--- src/pages/concierge/Concierge.jsx
+++ src/pages/concierge/Concierge.jsx
@@ -1,27 +1,56 @@
-import ConciergeList from '../concierge/concierge.json';
-import Table from './../../components/Table.jsx';
-import Image from './../../components/Image.jsx';
+import Table from "./../../components/Table.jsx";
+import Image from "./../../components/Image.jsx";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchConcierges } from "../../redux/features/concierge/conciergeSlice.js";
 function Concierge() {
+  const dispatch = useDispatch();
+
+  // Seleccionamos los datos desde el store
+  const { concierges, loading, error } = useSelector(
+    (state) => state.concierge
+  );
+
+  useEffect(() => {
+    dispatch(fetchConcierges());
+  }, [dispatch]);
+
   const cols = ["Name", "Job Desk", "Schedule", "Contact", "Status"];
 
   // Mapeamos los datos de ConciergeList
-  const data = ConciergeList.map((res) => ({
+  const data = concierges.map((res) => ({
     Name: (
       <div style={{ display: "flex", alignItems: "center" }}>
-        <Image src={res.photo} alt="Concierge" style={{ width: "50px", height: "50px", borderRadius: "50%" }} />
-        <div style={{ display: "flex", flexDirection: "column", marginLeft: "10px" }}>
-          <span>{res.first_name} {res.last_name}</span>
+        <Image
+          src={res.photo}
+          alt="Concierge"
+          style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginLeft: "10px",
+          }}
+        >
+          <span>
+            {res.first_name} {res.last_name}
+          </span>
           <span>#{res.id}</span>
           <span>{res.start_date}</span>
         </div>
       </div>
     ),
-    
+
     "Job Desk": res.function_description,
     Schedule: res.schedule,
     Contact: res.phone_number,
     Status: res.status ? "Active" : "Inactive",
   }));
+
+  if (loading) return <div>Loading concierges...</div>;
+  if (error) return <div>Error loading concierges: {error}</div>;
 
   return (
     <div style={{ padding: "20px" }}>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import guestsReducer from "./../features/guests/guestsSlice";
 import roomsReducer from "./../features/rooms/roomsSlice";
+import conciergeReducer from "./../features/concierge/conciergeSlice";
 
 const store = configureStore({
   reducer: {
     guest: guestsReducer, // Aquí pasamos el reducer correctamente
     room: roomsReducer, // Aquí pasamos el reducer correctamente
+    concierge: conciergeReducer, // Aquí pasamos el reducer correctamente
   },
 });
 

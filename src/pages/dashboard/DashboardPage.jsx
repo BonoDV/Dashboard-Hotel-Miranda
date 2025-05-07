e7dd1633c9e471b54223a8396f05a00dec8bf650
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-05-07 16:00:38 +0200

    feature: dashboard done

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -6,11 +6,32 @@ import styled from "styled-components";
 import KPI from "./../../components/KPI.jsx";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { FaArrowRight } from "react-icons/fa";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Monday", check_in: 50, check_out: 35 },
+  { name: "Tuesday", check_in: 70, check_out: 45 },
+  { name: "Wednesday", check_in: 40, check_out: 27 },
+  { name: "Thursday", check_in: 58, check_out: 45 },
+  { name: "Friday", check_in: 24, check_out: 11 },
+  { name: "Saturday", check_in: 76, check_out: 44 },
+  { name: "Sunday", check_in: 87, check_out: 45 },
+];
 
 const DashboardPage = () => {
   return (
     <Container>
-      {/* KPI group */}
       <KPIGroup>
         <KPI value={8461} label="New Booking" icon={<IoBedOutline />} />
         <KPI value={8461} label="Scheduled Room" icon={<BiCalendarCheck />} />
@@ -18,69 +39,172 @@ const DashboardPage = () => {
         <KPI value={8461} label="Check Out" icon={<BsBoxArrowInRight />} />
       </KPIGroup>
 
-      {/* Calendar */}
-      <StyledCalendar />
+      <div style={{ display: "flex", gap: "20px" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            width: "40%",
+            backgroundColor: "#ffffff",
+          }}
+        >
+          <StyledCalendar />
+          <RecentBookings>
+            <ItemContainer>
+              <ImagePlaceholder />
+              <BookingInfo>
+                <RoomName>Queen Bed A–12324</RoomName>
+                <GuestInfo>
+                  <GuestAvatar />
+                  <GuestDetails>
+                    <GuestName>James Sukardi</GuestName>
+                    <TimeAgo>8-5 ... 10-5</TimeAgo>
+                  </GuestDetails>
+                </GuestInfo>
+              </BookingInfo>
+              <DateBadge style={{ backgroundColor: "#004d40" }}>3</DateBadge>
+            </ItemContainer>
 
-      {/* Recent bookings - estático */}
-      <RecentBookings>
-        <ItemContainer>
-          <ImagePlaceholder />
-          <BookingInfo>
-            <RoomName>Queen Bed A–12324</RoomName>
-            <GuestInfo>
-              <GuestAvatar />
-              <GuestDetails>
-                <GuestName>James Sukardi</GuestName>
-                <TimeAgo>8-5 ... 10-5</TimeAgo>
-              </GuestDetails>
-            </GuestInfo>
-          </BookingInfo>
-          <DateBadge style={{ backgroundColor: "#004d40" }}>3</DateBadge>
-        </ItemContainer>
+            <ItemContainer>
+              <ImagePlaceholder />
+              <BookingInfo>
+                <RoomName>Deluxe Room B–1324</RoomName>
+                <GuestInfo>
+                  <GuestAvatar />
+                  <GuestDetails>
+                    <GuestName>Angela Moss</GuestName>
+                    <TimeAgo>8-5 ... 10-5</TimeAgo>
+                  </GuestDetails>
+                </GuestInfo>
+              </BookingInfo>
+              <DateBadge style={{ backgroundColor: "#e53935" }}>
+                16, 17, 18
+              </DateBadge>
+            </ItemContainer>
 
-        <ItemContainer>
-          <ImagePlaceholder />
-          <BookingInfo>
-            <RoomName>Deluxe Room B–1324</RoomName>
-            <GuestInfo>
-              <GuestAvatar />
-              <GuestDetails>
-                <GuestName>Angela Moss</GuestName>
-                <TimeAgo>8-5 ... 10-5</TimeAgo>
-              </GuestDetails>
-            </GuestInfo>
-          </BookingInfo>
-          <DateBadge style={{ backgroundColor: "#e53935" }}>
-            16, 17, 18
-          </DateBadge>
-        </ItemContainer>
+            <ItemContainer>
+              <ImagePlaceholder />
+              <BookingInfo>
+                <RoomName>King Big C–2445</RoomName>
+                <GuestInfo>
+                  <GuestAvatar />
+                  <GuestDetails>
+                    <GuestName>Geovanny</GuestName>
+                    <TimeAgo>8-5 ... 10-5</TimeAgo>
+                  </GuestDetails>
+                </GuestInfo>
+              </BookingInfo>
+              <DateBadge style={{ backgroundColor: "#ff9800" }}>20</DateBadge>
+            </ItemContainer>
 
-        <ItemContainer>
-          <ImagePlaceholder />
-          <BookingInfo>
-            <RoomName>King Big C–2445</RoomName>
-            <GuestInfo>
-              <GuestAvatar />
-              <GuestDetails>
-                <GuestName>Geovanny</GuestName>
-                <TimeAgo>8-5 ... 10-5</TimeAgo>
-              </GuestDetails>
-            </GuestInfo>
-          </BookingInfo>
-          <DateBadge style={{ backgroundColor: "#ff9800" }}>20</DateBadge>
-        </ItemContainer>
+            <ViewMore>View More</ViewMore>
+          </RecentBookings>
+        </div>
 
-        <ViewMore>View More</ViewMore>
-      </RecentBookings>
+        <div
+          style={{
+            width: "60%",
+            height: 650,
+            backgroundColor: "#ffffff",
+            borderRadius: "12px",
+            padding: "1rem",
+          }}
+        >
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              data={data}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="check_in" fill="#135846" name={"Check in"} />
+              <Bar dataKey="check_out" fill="#e23428" name={"Check out"} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+
+      <ReviewSection>
+        <ReviewHeader>
+          <SectionTitle>Latest Review by Customers</SectionTitle>
+          <NextButton>
+            <FaArrowRight />
+          </NextButton>
+        </ReviewHeader>
+
+        <ReviewCardsContainer>
+          <ReviewCard>
+            <ReviewText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </ReviewText>
+            <ReviewerInfo>
+              <ReviewerAvatar />
+              <ReviewerDetails>
+                <ReviewerName>Kusnaidi Anderson</ReviewerName>
+                <ReviewTime>4m ago</ReviewTime>
+              </ReviewerDetails>
+              <ActionButtons>
+                <ActionIcon approved />
+                <ActionIcon />
+              </ActionButtons>
+            </ReviewerInfo>
+          </ReviewCard>
+
+          <ReviewCard>
+            <ReviewText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </ReviewText>
+            <ReviewerInfo>
+              <ReviewerAvatar />
+              <ReviewerDetails>
+                <ReviewerName>Bella Saphira</ReviewerName>
+                <ReviewTime>4m ago</ReviewTime>
+              </ReviewerDetails>
+              <ActionButtons>
+                <ActionIcon approved />
+                <ActionIcon />
+              </ActionButtons>
+            </ReviewerInfo>
+          </ReviewCard>
+
+          <ReviewCard>
+            <ReviewText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </ReviewText>
+            <ReviewerInfo>
+              <ReviewerAvatar />
+              <ReviewerDetails>
+                <ReviewerName>Thomas Al–Ghazali</ReviewerName>
+                <ReviewTime>4m ago</ReviewTime>
+              </ReviewerDetails>
+              <ActionButtons>
+                <ActionIcon approved />
+                <ActionIcon />
+              </ActionButtons>
+            </ReviewerInfo>
+          </ReviewCard>
+        </ReviewCardsContainer>
+      </ReviewSection>
     </Container>
   );
 };
+
+// Styled Components
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
-  justify-content: flex-start;
 `;
 
 const KPIGroup = styled.div`
@@ -93,7 +217,7 @@ const KPIGroup = styled.div`
 const StyledCalendar = styled(Calendar)`
   border: none;
   border-radius: 12px;
-  width: 40%;
+  width: 100%;
   font-family: "Inter", sans-serif;
   padding: 1rem;
 `;
@@ -101,8 +225,9 @@ const StyledCalendar = styled(Calendar)`
 const RecentBookings = styled.div`
   display: flex;
   flex-direction: column;
-  width: 40%;
+  width: 90%;
   gap: 20px;
+  margin-left: 1.25rem;
 `;
 
 const ItemContainer = styled.div`
@@ -174,6 +299,104 @@ const ViewMore = styled.div`
   font-weight: 500;
   cursor: pointer;
   margin-top: 8px;
+`;
+
+const ReviewSection = styled.div`
+  background: #fff;
+  border-radius: 12px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ReviewHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SectionTitle = styled.h3`
+  margin: 0;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const ReviewCardsContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
+
+const ReviewCard = styled.div`
+  flex: 1;
+  min-width: 280px;
+  max-width: 32%;
+  background: #f9f9f9;
+  padding: 16px;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ReviewText = styled.p`
+  font-size: 14px;
+  color: #333;
+  flex: 1;
+`;
+
+const ReviewerInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 16px;
+  gap: 12px;
+`;
+
+const ReviewerAvatar = styled.div`
+  width: 40px;
+  height: 40px;
+  background: #ccc;
+  border-radius: 8px;
+`;
+
+const ReviewerDetails = styled.div`
+  flex-grow: 1;
+`;
+
+const ReviewerName = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const ReviewTime = styled.div`
+  font-size: 12px;
+  color: #aaa;
+`;
+
+const ActionButtons = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const ActionIcon = styled.div`
+  width: 20px;
+  height: 20px;
+  background: ${(props) => (props.approved ? "#00c853" : "#e53935")};
+  border-radius: 50%;
+`;
+
+const NextButton = styled.button`
+  background-color: #135846;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  width: 40px;
+  height: 40px;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `;
 
 export default DashboardPage;

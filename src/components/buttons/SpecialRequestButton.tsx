b1c8ba381b5071b900611a88c0f5a89c3ac36a0a
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-05-14 11:07:37 +0200

    feature: changed to tsx

--- src/components/buttons/SpecialRequestButton.tsx
+++ src/components/buttons/SpecialRequestButton.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const SpecialRequestButton = ({ specialRequest, onClick }) => {
+interface SpecialRequestButtonProps {
+  specialRequest?: string;
+  onClick?: () => void;
+}
+
+const SpecialRequestButton: React.FC<SpecialRequestButtonProps> = ({
+  specialRequest = "None Request",
+  onClick,
+}) => {
   const isNone = specialRequest === "None Request";
 
   return (
     <ButtonStyled
-      buttonStatus={specialRequest}
+      $buttonStatus={specialRequest}
       onClick={!isNone ? onClick : undefined}
     >
       {!isNone ? "View Request" : "None Request"}
@@ -15,7 +22,7 @@ const SpecialRequestButton = ({ specialRequest, onClick }) => {
   );
 };
 
-const getBackgroundColor = (status) => {
+const getBackgroundColor = (status: string): string => {
   switch (status) {
     case "None Request":
       return "#FFFFFF";
@@ -24,7 +31,7 @@ const getBackgroundColor = (status) => {
   }
 };
 
-const getTextColor = (status) => {
+const getTextColor = (status: string): string => {
   switch (status) {
     case "None Request":
       return "#cfd8d2";
@@ -33,7 +40,7 @@ const getTextColor = (status) => {
   }
 };
 
-const ButtonStyled = styled.button`
+const ButtonStyled = styled.button<{ $buttonStatus: string }>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -41,20 +48,12 @@ const ButtonStyled = styled.button`
   height: 65px;
   border-radius: 8px;
   border: none;
-  background-color: ${({ buttonStatus }) => getBackgroundColor(buttonStatus)};
-  color: ${({ buttonStatus }) => getTextColor(buttonStatus)};
-  cursor: ${({ buttonStatus }) =>
-    buttonStatus === "None Request" ? "not-allowed" : "pointer"};
+  background-color: ${({ $buttonStatus }) => getBackgroundColor($buttonStatus)};
+  color: ${({ $buttonStatus }) => getTextColor($buttonStatus)};
+  cursor: ${({ $buttonStatus }) =>
+    $buttonStatus === "None Request" ? "not-allowed" : "pointer"};
   font: normal normal 500 14px/21px Poppins;
 `;
 
-SpecialRequestButton.defaultProps = {
-  specialRequest: "None Request",
-};
-
-SpecialRequestButton.propTypes = {
-  specialRequest: PropTypes.string,
-  onClick: PropTypes.func,
-};
-
 export default SpecialRequestButton;
+
